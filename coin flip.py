82343ce0c2126e126219d765ef5337d54fd21496
Carson Beasley <62185219+carsonbeasley@users.noreply.github.com>
2021-12-03 14:09:45 -0500

    Coin Flip
    This is an example of Codecademy's coin flip python project.

---
+++ coin flip.py
@@ -0,0 +1,24 @@
+import random
+
+money = 100
+#defines starting balance
+
+def coin_flip(call, amount):
+    if random.randint(1,2) == 1:
+        coin_toss = "Heads"
+    else:
+        coin_toss = "Tails"
+    if call == coin_toss:
+        result = "won"
+        total_amount = money + amount
+    else:
+        result = "lost"
+        total_amount = money - amount
+        
+                
+    print(
+        "You flipped a %s and you called %s! You %s and now have $%.2f"
+        %(coin_toss, call, result, total_amount)
+        )
+               
+
